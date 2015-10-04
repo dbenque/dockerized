b67f1026b3e131ce71699d60f6b11a797cf7eddf
David Benque <dbenque@gmail.com>
2015-10-04 18:07:58 +0200

    add eclipse GWT image

---
+++ eclipseGWT/eclipse.sh
@@ -0,0 +1,5 @@
+xhost +
+docker run --net=host -v ~/code/eclipse/workspace/:/home/eclipseuser/workspace \
+  -v ~/code/eclipse/.home:/home/eclipseuser \
+  -e DISPLAY  -e "UID=`id -u $USER`" -e "GUID=`id -g $USER`" -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+  -d eclipse

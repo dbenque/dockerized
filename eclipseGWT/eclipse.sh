51ee26e96728e6b21d575897cd26f100811f3796
David Benque <dbenque@gmail.com>
2015-12-27 22:10:38 +0100

    sgdb plugin

--- eclipseGWT/eclipse.sh
+++ eclipseGWT/eclipse.sh
@@ -1,4 +1,6 @@
 xhost +
+mkdir -p ~/code/eclipse/workspace/
+mkdir -p ~/code/eclipse/.home
 docker run --net=host -v ~/code/eclipse/workspace/:/home/eclipseuser/workspace \
   -v ~/code/eclipse/.home:/home/eclipseuser \
   -e DISPLAY  -e "UID=`id -u $USER`" -e "GUID=`id -g $USER`" -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
